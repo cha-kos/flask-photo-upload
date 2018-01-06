902bdec8277ce8157685a0f9246fb12732e9108d
Chris Hakos <chrishakos@gmail.com>
2018-01-06 18:51:56 -0500

    convert test_image_stream for assert statement

--- photo_upload_test.py
+++ photo_upload_test.py
@@ -19,12 +19,12 @@ class UploadTestCase(unittest.TestCase):
     def test_photo_upload(self):
 
         with open('test_files/test_image.jpg') as test:
-            test_image = StringIO.StringIO(test.read())
-            test.close()
+            test_image_stream = test.read()
+            test_image_object = StringIO.StringIO(test_image_stream)
 
-        upload = self.app.post('/upload', data = {'file': (test_image, 'test.jpg')}, follow_redirects=True)
-        converted_test_image = test_image.encode('base64').replace('\n', '')
-        assert converted_test_image in upload.data
+        upload = self.app.post('/upload', data = {'file': (test_image_object, 'test.jpg')}, follow_redirects=True)
+        converted_test_image_stream = test_image_stream.encode('base64').replace('\n', '')
+        assert converted_test_image_stream in upload.data
 
 if __name__ == '__main__':
     unittest.main()
